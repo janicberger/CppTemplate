403e7951c627de0acbb901a8bed6cee384d6d685
janicberger <janic.berger@students.fhnw.ch>
2026-03-20 14:54:10 +0100

    clang tidy test

--- apps/cli/main.cpp
+++ apps/cli/main.cpp
@@ -3,9 +3,14 @@
 
 #include <CLI/CLI.hpp>
 
+void foo() {
+    foo();
+}
+
 int main() {
+    std::string name = "user";
     if (true) {
-        fmt::println("Greeter: {}", myProject::core::Greeter::greet("user"));
+        fmt::println("Greeter: {}", myProject::core::Greeter::greet(name));
     } else {
         return 1;
     }
