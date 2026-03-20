673c4af2700d89ad3a1932532d9d4e3e3cf35a80
janicberger <janic.berger@students.fhnw.ch>
2026-03-20 14:20:21 +0100

    clang tidy test

--- apps/cli/main.cpp
+++ apps/cli/main.cpp
@@ -1,6 +1,8 @@
 #include <fmt/format.h>
 #include <prj/core/greeter.h>
 
+#include <CLI/CLI.hpp>
+
 int main() {
     fmt::println("Greeter: {}", myProject::core::Greeter::greet("user"));
     return 0;
