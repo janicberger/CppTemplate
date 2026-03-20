b3068880623929b2951073d1a607c8a86a573605
janicberger <janic.berger@students.fhnw.ch>
2026-03-20 15:13:56 +0100

    CLI11 usage

--- apps/cli/main.cpp
+++ apps/cli/main.cpp
@@ -1,18 +1,17 @@
 #include <fmt/format.h>
-#include <prj/core/greeter.h>
-
 #include <CLI/CLI.hpp>
 
-void foo() {
-    foo();
-}
+#include <prj/core/greeter.h>
 
-int main() {
+int main(const int argc, char **argv) {
+    CLI::App app {"Greeter CLI App"};
+    argv = app.ensure_utf8(argv);
+
     std::string name = "user";
-    if (true) {
-        fmt::println("Greeter: {}", myProject::core::Greeter::greet(name));
-    } else {
-        return 1;
-    }
+    app.add_option("-n,--name", name, "Your name.");
+
+    CLI11_PARSE(app, argc, argv);
+
+    fmt::println("Greeter: {}", myProject::core::Greeter::greet(name));
     return 0;
 }
