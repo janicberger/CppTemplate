87c32ba0c1beff7d77090b53eddeaf83880b18d8
janicberger <janic.berger@students.fhnw.ch>
2026-03-26 15:36:45 +0100

    0.1.0

--- libs/core/include/prj/core/greeter.h
+++ libs/core/include/prj/core/greeter.h
@@ -1,11 +1,9 @@
 #pragma once
-#include <iostream>
 #include <string>
 
 namespace myProject::core {
     class Greeter {
     public:
-        static void yo() { std::cout << "YOOOO" << std::endl; }
         static std::string greet(const std::string& name);
     };
 }  // namespace myProject::core
